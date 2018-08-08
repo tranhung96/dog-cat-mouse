8348264cc8744f909f5c1dafe1855d544dd37759
codertokyo <tranhung.ptit>
2018-08-08 09:05:49 +0700

    improve method for cat

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var dog=require('./doc');
 var cat=require('./cat');
 var mig= new dog('tom');
+var mouse=require('./mouse');
 mig.say();
 var  tom=new cat();
-mig.eat(tom);
-console.log(mig);
+//mig.eat(tom);
+//console.log(mig);
 //console.log(tom);
+var Mouse=new mouse('jerry');
+try{
+tom.eat(mig);
+}
+catch(error){
+	console.log('error while cat eating mig');
+
+}
+console.log(tom);
